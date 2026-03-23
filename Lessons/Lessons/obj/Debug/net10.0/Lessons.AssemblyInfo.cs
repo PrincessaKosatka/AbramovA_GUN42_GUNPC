0dcf0b10be8d0f5c626702c80964d319cbf35f90
PrincessaKosatka <bender1295@gmail.com>
2026-03-23 18:42:30 +0300

    Index more files for Homework

--- Lessons/Lessons/obj/Debug/net10.0/Lessons.AssemblyInfo.cs
+++ Lessons/Lessons/obj/Debug/net10.0/Lessons.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lessons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+58633fd0eeeee6afcd75693a0d6c28f91cb616d4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aa82cc4da5df529a12381af139ed156177bc8f4")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lessons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lessons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
